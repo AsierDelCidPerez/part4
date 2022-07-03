fd54ad2b5de06e0355971e5644055619a6a46415
Asier Del Cid Pérez <103944663+AsierDelCidPerez@users.noreply.github.com>
2022-07-03 12:03:28 +0200

    Update mostLikes.test.js

--- backend2/tests/mostLikes.test.js
+++ backend2/tests/mostLikes.test.js
@@ -1,9 +1,6 @@
 const mostLikes = require('../utils/list_helper').mostLikes
 const blogs = require('./blogs')
 
-
-
-mostLikes(blogs)
 test('Most likes', () => {
     expect(mostLikes(blogs)).toEqual({ author: 'Edsger W. Dijkstra', likes: 17 })
 })
